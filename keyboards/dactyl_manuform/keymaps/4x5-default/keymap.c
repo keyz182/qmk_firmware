53116d538d9703981c190dc72d5fb18866734781
Kieran David Evans <keyz182@gmail.com>
2024-10-04 13:58:18 +0100

    Working firmware for dactyl manuform 4x5

--- keyboards/dactyl_manuform/keymaps/4x5-default/keymap.c
+++ keyboards/dactyl_manuform/keymaps/4x5-default/keymap.c
@@ -20,13 +20,13 @@ extern keymap_config_t keymap_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = KEYMAP_4x5(
   // left hand
-   KC_Q,    KC_W,   KC_E, KC_R,  KC_T,
-   KC_A,    KC_S,   KC_D, KC_F,  KC_G,
-   KC_Z,    KC_X,   KC_C, KC_V,  KC_B,
-            KC_GRV, KC_TAB,
-                               LT(_SYM, KC_DEL), SFT_T(KC_ENT),
-                               LT(_MOVE, KC_SPC), CTL_T(KC_ESC),
-                               KC_BTN1, LT(_FN, KC_TAB),
+   KC_T,    KC_R,   KC_E, KC_W,  KC_Q,
+   KC_G,    KC_F,   KC_D, KC_S,  KC_A,
+   KC_B,    KC_V,   KC_C, KC_X,  KC_Z,
+            KC_TAB, KC_GRV,
+                               SFT_T(KC_ENT), LT(_SYM, KC_DEL),
+                               CTL_T(KC_ESC), LT(_MOVE, KC_SPC),
+                               LT(_FN, KC_TAB), KC_BTN1,
         // right hand
                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
                      KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
@@ -37,13 +37,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LALT, KC_DQT),
 [_SYM] = KEYMAP_4x5(
   // left hand
-   KC_PIPE, KC_EXLM, KC_LCBR, KC_RCBR, KC_AT,
-   KC_AMPR, KC_HASH, KC_LPRN, KC_RPRN, KC_DLR,
-   KC_SCLN, KC_PERC, KC_LBRC, KC_RBRC, KC_BSLS,
-            KC_GRV,  KC_TAB,
-                               LT(_SYM, KC_DEL), SFT_T(KC_ENT),
-                               LT(_MOVE, KC_SPC), CTL_T(KC_ESC),
-                               KC_BTN1, LT(_FN, KC_TAB),
+   KC_PIPE, KC_RCBR, KC_LCBR, KC_EXLM, KC_PIPE,
+   KC_DLR,  KC_RPRN, KC_LPRN, KC_HASH, KC_AMPR,
+   KC_BSLS, KC_RBRC, KC_LBRC, KC_PERC, KC_SCLN,
+            KC_TAB, KC_GRV,
+                               SFT_T(KC_ENT), LT(_SYM, KC_DEL),
+                               CTL_T(KC_ESC), LT(_MOVE, KC_SPC),
+                               LT(_FN, KC_TAB), KC_BTN1,
         // right hand
                      KC_PLUS, KC_7, KC_8, KC_9, KC_CIRC,
                      KC_EQL,  KC_4, KC_5, KC_6, KC_ASTR,
@@ -54,13 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LALT, KC_DQT),
 [_MOVE] = KEYMAP_4x5(
   // left hand
-   QK_REBOOT,   KC_WH_U, KC_MS_U, KC_WH_D,  KC_WH_U,
-   KC_BTN3, KC_MS_L, KC_MS_D, KC_MS_R,  KC_LGUI,
-   KC_BTN1, KC_BTN5, KC_BTN2, KC_BTN3,  KC_WH_D,
-            KC_GRV,  KC_TAB,
-                               LT(_SYM, KC_DEL), SFT_T(KC_ENT),
-                               LT(_MOVE, KC_SPC), CTL_T(KC_ESC),
-                               KC_BTN1, LT(_FN, KC_TAB),
+   KC_WH_U, KC_WH_D, KC_MS_U, KC_WH_U,  QK_REBOOT,
+   KC_LGUI, KC_MS_R, KC_MS_D, KC_MS_L,  KC_BTN3,
+   KC_WH_D, KC_BTN3, KC_BTN2, KC_BTN5,  KC_BTN1,
+            KC_TAB, KC_GRV,
+                               SFT_T(KC_ENT), LT(_SYM, KC_DEL),
+                               CTL_T(KC_ESC), LT(_MOVE, KC_SPC),
+                               LT(_FN, KC_TAB), KC_BTN1,
         // right hand
                      KC_HOME, KC_PGUP, KC_PGDN, KC_END, _______,
                      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, LALT(KC_TAB),
@@ -71,13 +71,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LALT, KC_DQT),
 [_FN] = KEYMAP_4x5(
   // left hand
-   KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_WH_U,
-   KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_LGUI,
-   KC_F9, KC_F10,  KC_F11, KC_F12, KC_WH_D,
-            KC_GRV,  KC_TAB,
-                               LT(_SYM, KC_DEL), SFT_T(KC_ENT),
-                               LT(_MOVE, KC_SPC), CTL_T(KC_ESC),
-                               KC_BTN1, LT(_FN, KC_TAB),
+   KC_WH_U,  KC_F4,  KC_F3,  KC_F2,  KC_F1,
+   KC_LGUI,  KC_F8,  KC_F7,  KC_F6,  KC_F5,
+   KC_WH_D,  KC_F12, KC_F11, KC_F10, KC_F9,
+            KC_TAB, KC_GRV,
+                               SFT_T(KC_ENT), LT(_SYM, KC_DEL),
+                               CTL_T(KC_ESC), LT(_MOVE, KC_SPC),
+                               LT(_FN, KC_TAB), KC_BTN1,
         // right hand
                      _______, _______, _______, _______, _______,
                      KC_CALC, KC_MAIL, KC_MYCM, KC_WHOM, _______,
